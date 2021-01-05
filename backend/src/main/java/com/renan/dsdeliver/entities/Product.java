f1c0b1615100432d9f230993071ca8f3f780c0a4
Renan Muniz <fmrenan@outlook.com>
2021-01-04 22:12:31 -0300

    /GET Orders

--- backend/src/main/java/com/renan/dsdeliver/entities/Product.java
+++ backend/src/main/java/com/renan/dsdeliver/entities/Product.java
@@ -11,12 +11,9 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "tb_product")
 public class Product implements Serializable{
-	
-	/**
-	*/ 
+	///
 	private static final long serialVersionUID = 1L;
-	/**
-	*/
+	///
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
