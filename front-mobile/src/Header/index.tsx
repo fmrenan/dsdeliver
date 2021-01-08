d4abc71e2daec9fd9009d651631e97507f118125
Renan Muniz <fmrenan@outlook.com>
2021-01-08 19:18:36 -0300

    Added Homepage

---
+++ front-mobile/src/Header/index.tsx
@@ -0,0 +1,34 @@
+import { OpenSans_700Bold } from '@expo-google-fonts/open-sans';
+import React from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
+
+export default function Header() {
+  return (
+    <View style={styles.container}>
+      
+      <Image source={require('../assets/logo.png')} />
+      <Text style={styles.text}>DS Deliver</Text>
+     
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#da5c5c',
+    height: 90,
+    paddingTop: 50,
+    flexDirection: 'row',
+    justifyContent: 'center'
+  },
+  text:{
+    fontWeight: 'bold',
+    fontSize: 18,
+    fontFamily: 'OpenSans_700Bold',
+    lineHeight: 25,
+    letterSpacing: -0.24,
+    color: '#FFF',
+    marginLeft: 15
+  },
+
+});
