5ace93ee5f1fb8ffb7cf74837519fd4dd5fdc9b4
Renan Muniz <fmrenan@outlook.com>
2021-01-08 10:23:08 -0300

    Search order location

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -1,7 +1,12 @@
 import axios from "axios";
 
 const base_url = 'https://dsdeliverbackend.herokuapp.com';
+const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAPBOX;
 
 export function fetchProducts(){
   return axios(`${base_url}/products`)
+}
+
+export function fetchLocalMapBox(local: string){
+  return axios(`https://api.mapbox.com/geocoding/v5/mapbox.places/${local}.json?access_token=${mapboxToken}`);
 }
