1c0bc02950429f925315a5423080af0099967a65
Renan Muniz <fmrenan@outlook.com>
2021-01-08 12:17:44 -0300

    Fixed env variables

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { OrderPayload } from "./Orders/types";
 
-const base_url = 'https://dsdeliverbackend.herokuapp.com';
+const base_url = process.env.REACT_APP_API_URL;
 const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAPBOX;
 
 export function fetchProducts(){
